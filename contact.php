bac30d780851e6f37a262cfc6ad8a823834e1af0
OddFutureDaniel <danielmatt@hotmail.co.uk>
2022-09-12 16:20:42 +0100

    contact form

--- contact.php
+++ contact.php
@@ -1,8 +1,28 @@
-<html>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <link type="text/css" rel="stylesheet" href="style.css" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Contact form</title>
+</head>
+
 <body>
 
-Welcome <?php echo $_POST["name"]; ?><br>
-Your email address is: <?php echo $_POST["email"]; ?>
+    <?php
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+    $formcontent = " From: $name \n Message: $message";
+    $recipient = "danielmatt@hotmail.co.uk";
+    $subject = "Contact Form";
+    $mailheader = "From: $email \r\n";
+    mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
+    echo "Thank You!";
+    ?>
 
 </body>
+
 </html>
